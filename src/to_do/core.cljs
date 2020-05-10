3686d1ca7195fcac952197cc1b9df497dbaed062
Yuhri Bernardes <yuhri.bernardes@paygo.com.br>
2020-05-10 12:46:34 -0300

    Little refactor in project entry point

--- src/to_do/core.cljs
+++ src/to_do/core.cljs
@@ -1,8 +1,13 @@
 (ns to-do.core
   (:require [reagent.dom :as dom]))
 
+(defn index []
+  [:div.main
+   [:div "input place"]
+   [:div "Task place"]])
+
 (defn main []
-  (dom/render [:div "hello"]
+  (dom/render [index]
               (.querySelector js/document "#app")))
 
 (defn ^:dev/after-load force-render []
